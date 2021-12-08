7ef928623fbb89097e9634ec0bcd87541d013df5
Сергей Попович <cr4nkle@yandex.ru>
2021-12-08 20:31:54 +0800

    Добавил цикл для создания большого кол-ва частиц.

--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -12,10 +12,21 @@ namespace CourseWork
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
+
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                
+                particles.Add(particle);
+            }
 
         }
 
