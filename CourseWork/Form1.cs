7f11f119b5fe9a7eef3811975cd5283e38eb6338
Сергей Попович <cr4nkle@yandex.ru>
2021-12-08 22:33:59 +0800

    Передел на векторную скорость.

--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -35,18 +35,20 @@ namespace CourseWork
                 particle.Life--;
                 if(particle.Life < 0)
                 {
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
                     particle.Life = 20 + Particle.rand.Next(100);
                     particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particle.Direction = Particle.rand.Next(360);
-                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Y = MousePositionY;                   
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
                     particle.Radius = 2 + Particle.rand.Next(10);
                 }
                 else
                 {
-                    var directionInRadians = particle.Direction / 180 * Math.PI;
-                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
                 }                
             }
 
