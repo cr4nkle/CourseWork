6891111996eee912d51aa5c27a3b5f3b3a68d141
Сергей Попович <cr4nkle@yandex.ru>
2021-12-08 21:18:45 +0800

    Добавил новое поле в класс Particle.

--- CourseWork/Particle.cs
+++ CourseWork/Particle.cs
@@ -14,6 +14,7 @@ namespace CourseWork
         public float Y; 
         public float Direction; 
         public float Speed;
+        public float Life;
 
         public static Random rand = new Random();
 
@@ -22,12 +23,13 @@ namespace CourseWork
             Radius = 2 + rand.Next(10);
             Direction = rand.Next(360);
             Speed = 1 + rand.Next(10);
-            
+            Life = 20 + rand.Next(100);
+
         }
 
         public void Draw(Graphics g)
         {
-            var brush = new SolidBrush(Color.Black);
+            var brush = new SolidBrush(Color.OrangeRed);
             g.FillEllipse(brush, X - Radius, Y - Radius, Radius * 2, Radius * 2);
             brush.Dispose();
         }
