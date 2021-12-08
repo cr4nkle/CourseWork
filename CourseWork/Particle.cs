d48917d7927eb7994edcb95c646a803424ea9139
Сергей Попович <cr4nkle@yandex.ru>
2021-12-08 20:23:24 +0800

    Добавил поля для класса Particle.

--- CourseWork/Particle.cs
+++ CourseWork/Particle.cs
@@ -8,5 +8,10 @@ namespace CourseWork
 {
     class Particle
     {
+        public int Radius; 
+        public float X; 
+        public float Y; 
+        public float Direction; 
+        public float Speed;
     }
 }
